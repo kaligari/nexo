02e7c69709ee2471b1c066c75135d974698e252c
Sebastian Kopeć <github.com@soften.pl>
2020-04-30 14:34:04 +0200

    Rebuild for build dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,17 +5,15 @@ mix.options({
 	processCssUrls: false
 });
 
-mix.sass('./src/main.sass', './dist/nexo.min.css')
-.js('./src/docs/main.js', './docs/docs.js')
-.copy('./dist/nexo.min.css', './docs/nexo.min.css')
-
-// mix.webpackConfig({
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.jade$/,
-// 				loader: 'pug-plain-loader'
-// 			}
-// 		]
-// 	}
-// })
+if (mix.inProduction()) {
+	mix
+	.sass('./src/main.sass', './build/nexo.min.css')
+	.copy('./src/docs/index.html', './docs/index.html')
+	.copy('./build/nexo.min.css', './demo/nexo.min.css')
+	.js('./src/docs/main.js', './demo/docs.js')
+} else {
+	mix
+	.copy('./src/docs/index.html', './dist/index.html')
+	.sass('./src/main.sass', './dist/nexo.min.css')
+	.js('./src/docs/main.js', './dist/docs.js')
+}
