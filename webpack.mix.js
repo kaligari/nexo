3284d4935ee3d38064e74ecac3739a5dd4c37889
Sebastian Kopeć <github.com@soften.pl>
2020-05-02 12:05:31 +0200

    Folder refactor

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,14 +9,16 @@ mix.options({
 });
 
 if (mix.inProduction()) {
+	// build
 	mix
-	.sass('./src/main.sass', './build/nexo.min.css')
+	.sass('./src/nexo/main.sass', './build/nexo.min.css')
 	.copy('./src/docs/index.html', './docs/index.html')
 	.copy('./build/nexo.min.css', './docs/nexo.min.css')
 	.js('./src/docs/main.js', './docs/docs.js')
 } else {
+	// watch
 	mix
 	.copy('./src/docs/index.html', './dist/index.html')
-	.sass('./src/main.sass', './dist/nexo.min.css')
+	.sass('./src/nexo/main.sass', './dist/nexo.min.css')
 	.js('./src/docs/main.js', './dist/docs.js')
 }
