b59d7fa862b8c96f1ebd6b7625f8e62bb0f43a0c
Sebastian Kopeć <github.com@soften.pl>
2020-05-12 21:47:25 +0200

    Molecule m-content

--- src/docs/views/Home.vue
+++ src/docs/views/Home.vue
@@ -1,9 +1,40 @@
 <template>
     <section class="l-section">
         <div class="l-container">
-            <div class="l-row">
-                <div class="l-row__column --textAlign-center">
-                    <h1 class="a-title">Nexo CSS</h1>
+            <div class="l-row --justifyContent-center">
+                <div class="l-row__column l-row__column--8 --textAlign-center">
+                    <div class="m-content">
+                        <h1 class="a-title">Nexo CSS</h1>
+                        <h2 class="a-title a-title--subtitle">Nexo CSS Framework</h2>
+                        <p>
+                            CSS framework based on modern technologies: Flexbox with BEM and Atomic Design methodologies.
+                            Checkout our documentation: https://kaligari.github.io/nexo-css/
+                        </p>
+                    </div>
+                    <div class="m-content">
+                        <h2 class="a-title a-title--subtitle">Main features:</h2>
+                        <p>
+                            <ul>
+                                <li>BEM combined with Atomic Design naming conventions (e.g **.a-message__header--large**)</li>
+                                <li>Flexbox</li>
+                                <li>CSS properties (variables)</li>
+                                <li>Mobile first</li>
+                                <li>Lightweight</li>
+                                <li>Modificators' classes (e.g **.--noBorder**)</li>
+                            </ul>
+
+                        </p>
+                    </div>
+                    <div class="m-content">
+                        <h2 class="a-title a-title--subtitle">Main advantages:</h2>
+                        <p>
+                            <ul>
+                                <li>easy and fast development and maintaining of code</li>
+                                <li>modularity</li>
+                                <li>customizing using CSS properties</li>
+                            </ul>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
